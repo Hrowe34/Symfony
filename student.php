12b857872449474f6788ef59468891235b83617f
Hrowe34 <45744655+Hrowe34@users.noreply.github.com>
2019-04-09 00:15:52 -0400

    Delete student.php

--- student.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace CarrollCommunityCollege\People;
-use PHPUnit\Runner\Exception;
-class Student {
-    function learn() {
-        echo "I Dont have a clue what to do!\n";
-        //throw new Exception('HIIIIIIIII.');
-    }
-}
